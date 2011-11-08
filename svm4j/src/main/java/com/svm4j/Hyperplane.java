57efc2e8d3dfeed5d0d87658f18b0cf0fd0a5091
twizansk <twizansk@e3987dcf-aadb-b242-4e9f-e44c8d0b67cb>
2011-11-08 17:56:42 +0000

    Cosmetic changes

--- svm4j/src/main/java/com/svm4j/Hyperplane.java
+++ svm4j/src/main/java/com/svm4j/Hyperplane.java
@@ -1,45 +1,56 @@
 package com.svm4j;
 
 /**
- * Represents hyperplane in a multidimensional space.  The points on the hyperplane satisfy:
- * w . x + b = 0
- *  
+ * Represents hyperplane in a multidimensional space. The points on the
+ * hyperplane satisfy: w . x + b = 0
+ * 
  * @author Tommer
- *
+ * 
  */
-public class Hyperplane 
-{
+public class Hyperplane {
 	double[] w; // The vector perpendicular to the hyperplane.
 	double b; // The offset parameter
-	
-	public Hyperplane() 
-	{
+
+	public Hyperplane() {
 		super();
 	}
 
-	public Hyperplane(double[] w, double b)
-	{
+	public Hyperplane(double[] w, double b) {
 		this.w = w;
 		this.b = b;
 	}
 
-	public double[] getW() 
-	{
+	/**
+	 * Classify a point based on the model. The point is assigned +1 if
+	 * x * w + b > 0 and -1 if x * w + b < 0
+	 * 
+	 * @param x
+	 * @return
+	 */
+	public double classify(double[] x) {
+		if (x.length != w.length) {
+			throw new RuntimeException("The given point is of the wrong dimension");
+		}
+		double result  = b;
+		for (int i = 0; i < w.length; i++) {
+			result += w[i] * x[i];
+		}
+		return result > 0 ? 1.0 : -1.0;	
+	}
+
+	public double[] getW() {
 		return w;
 	}
 
-	public void setW(double[] w) 
-	{
+	public void setW(double[] w) {
 		this.w = w;
 	}
 
-	public double getB() 
-	{
+	public double getB() {
 		return b;
 	}
 
-	public void setB(double b) 
-	{
+	public void setB(double b) {
 		this.b = b;
-	}	
+	}
 }
